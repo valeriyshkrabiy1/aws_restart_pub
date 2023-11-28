4b72680efabbf082c103a770a393829f63a06ff1
Valeriy Shkrabiy <40370862+valeriyshkrabiy1@users.noreply.github.com>
2023-11-28 14:09:35 +0200

    Add files via upload

---
+++ helloWorld1.py
@@ -0,0 +1,10 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
